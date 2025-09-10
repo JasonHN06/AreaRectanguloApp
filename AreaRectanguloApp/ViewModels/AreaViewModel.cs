8c81e8ed1456049e16800b0df23be167df203859
Jason Madrid <jasonhnn6@gmail.com>
2025-09-10 15:20:31 -0600

    create viewmodel

---
+++ AreaRectanguloApp/ViewModels/AreaViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace AreaRectanguloApp.ViewModels
+{
+    public partial class AreaViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private double baseRectangulo;
+
+        [ObservableProperty]
+        private double alturaRectangulo;
+
+        [ObservableProperty]
+        private double resultado;
+
+        [RelayCommand]
+        private void CalcularArea()
+        {
+            Resultado = BaseRectangulo * AlturaRectangulo;
+        }
+
+        [RelayCommand]
+        private void LimpiarArea()
+        {
+            BaseRectangulo = 0;
+            AlturaRectangulo = 0;
+            Resultado = 0;
+        }
+    }
+}
